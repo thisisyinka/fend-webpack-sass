d2462dc75841a3ba0531c6d2c4cdf5241843fff8
Layinka Jaji <layinkawebdev@gmail.com>
2021-09-02 13:58:33 +0100

    API Call Challenge in Lesson 4

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,12 +5,24 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    // console.log("::: Form Submitted :::")
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    let city = 'Lagos';
+    const apiKey = 'API-KEY';
+
+    fetch(`${baseURL}${city}&appid=${apiKey}`)
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    .then(
+        function(data) {
+            const type = data.weather.map(param => param.main);
+            document.getElementById('results').innerHTML = type;
+        }
+    )
 }
 
 export { handleSubmit }
